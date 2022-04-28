45d6309d11dfdc41b4a8a6d17a9b70f7b53191e2
Lincoln Thai <lincoln.thai.gamedesign@gmail.com>
2022-04-28 12:19:26 +1000

    Player Camera Script Completed

---
+++ Assets/NASAnal Space Station/Scripts/PlayerCamera.cs
@@ -0,0 +1,49 @@
+namespace NASAnalSpaceStation
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class PlayerCamera : MonoBehaviour
+    {
+        #region Fields
+
+        // Set Variables
+        public float mouseSensitivity = 100f;
+
+        public Transform playerBody;
+
+        float xRotation = 0f;
+
+        #endregion
+
+        #region Unity Methods
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Get Mouse Movement Input multiply by sensitivity and time
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+            // assign mouseY input to a variable for clamping
+            xRotation -= mouseY;
+            // clamp x rotation between 90 and -90 degrees
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+            // rotate camera up or down based on its own transform
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            // rotate the body with the camera for left and right rotation
+            playerBody.Rotate(Vector3.up * mouseX);
+
+        }
+
+        #endregion
+    }
+}
