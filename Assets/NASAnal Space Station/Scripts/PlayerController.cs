6af25c8259449ea0da227a8a866fc0595f7cecd9
LincThai <lincoln.thai.gamedesign@gmail.com>
2022-04-29 13:10:40 +1000

    Rigidbody Land Movement

---
+++ Assets/NASAnal Space Station/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+namespace NASAnalSpaceStation
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class PlayerController : MonoBehaviour
+    {
+        // set up movement variables
+        [Header("Movement")]
+        // set speed
+        public float moveSpeed = 6f;
+
+        // set drag variable
+        public float rbDrag = 6f;
+
+        // set horizontal and vertical movement variables
+        float horizontalMove;
+        float verticalMove;
+
+        // set up a vector 3 variable for movement direction
+        Vector3 moveDirection;
+
+        // variable to assign the riid body to
+        Rigidbody rb;
+
+        private void Start()
+        {
+            // assigns the rigid body to rb
+            rb = GetComponent<Rigidbody>();
+            rb.freezeRotation = true;
+        }
+
+        private void Update(){
+            // Call MyInput function
+            MyInput();
+
+            // call ControlDrag Function
+        }
+
+        void MyInput(){
+            // collect input data
+            horizontalMove = Input.GetAxisRaw("Horizontal");
+            verticalMove = Input.GetAxisRaw("Vertical");
+
+            // move in the direction relative to the direction of sight
+            moveDirection = transform.forward * verticalMove + transform.right * horizontalMove;
+        }
+
+        void ControlDrag(){
+            rb.drag = rbDrag;
+        }
+
+        private void FixedUpdate(){
+            // call MovePlayer function
+            MovePlayer();
+        }
+
+        void MovePlayer(){
+            // add a normalized force multiplied by the movement speed as an acceleration
+            rb.AddForce(moveDirection.normalized * moveSpeed, ForceMode.Acceleration);
+        }
+    }
+}
