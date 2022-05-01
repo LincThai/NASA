7a55ed9e09425dd011c18f0c5682f5a8e7717646
Lincoln Thai <lincoln.thai.gamedesign@gmail.com>
2022-05-01 17:33:15 +1000

    Working on Rigidbody Jump

--- Assets/NASAnal Space Station/Scripts/PlayerController.cs
+++ Assets/NASAnal Space Station/Scripts/PlayerController.cs
@@ -14,9 +14,15 @@ namespace NASAnalSpaceStation
         // set drag variable
         public float rbDrag = 6f;
 
+        [Header("Jump")]
+        public float JumpForce = 5f;
+
         // set horizontal and vertical movement variables
         float horizontalMove;
         float verticalMove;
+
+        // ground check
+        bool isGrounded;
 
         // set up a vector 3 variable for movement direction
         Vector3 moveDirection;
@@ -32,10 +38,19 @@ namespace NASAnalSpaceStation
         }
 
         private void Update(){
+            isGrounded = Physics.Raycast(transform.position, Vector3.down, 0.2f);
+            Debug.Log(isGrounded);
+
             // Call MyInput function
             MyInput();
 
             // call ControlDrag Function
+            ControlDrag();
+
+            if (Input.GetKeyDown("Jump") && isGrounded)
+            {
+                Jump();
+            }
         }
 
         void MyInput(){
@@ -45,6 +60,11 @@ namespace NASAnalSpaceStation
 
             // move in the direction relative to the direction of sight
             moveDirection = transform.forward * verticalMove + transform.right * horizontalMove;
+        }
+
+        void Jump()
+        {
+            rb.AddForce(transform.up * JumpForce, ForceMode.Impulse);
         }
 
         void ControlDrag(){
