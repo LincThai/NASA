da2a7cd14674f1ed2c3d7ab55883bff627d99236
Lincoln Thai <lincoln.thai.gamedesign@gmail.com>
2022-05-02 16:27:01 +1000

    Game Manager work for Zero-G mode

--- Assets/NASAnal Space Station/Scripts/GameManager.cs
+++ Assets/NASAnal Space Station/Scripts/GameManager.cs
@@ -1,18 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GameManager : MonoBehaviour
+namespace NASAnalSpaceStation
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
 
-    // Update is called once per frame
-    void Update()
+    public class GameManager : MonoBehaviour
     {
-        
+        #region Fields
+
+        // Set Variables
+        public GameState gameState;
+
+        #endregion
+        // Start is called before the first frame update
+        void Start()
+        {
+            gameState = GameState.preGame;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
+    public enum GameState { preGame, game, dead, Pause }
+    public enum PlayerState { gravity , zeroGravity }
 }
