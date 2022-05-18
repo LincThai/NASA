4e90a07fa807a938b9de37a67e5ab3c05a90d864
Lincoln Thai <lincoln.thai.gamedesign@gmail.com>
2022-05-18 18:03:27 +1000

    Game End popup

--- Assets/NASAnal Space Station/Scripts/GameEndPopUp.cs
+++ Assets/NASAnal Space Station/Scripts/GameEndPopUp.cs
@@ -3,21 +3,33 @@ namespace NASAnalSpaceStation
     using System.Collections;
     using System.Collections.Generic;
     using UnityEngine;
+    using TMPro;
 
     public class GameEndPopUp : MonoBehaviour
     {
+        #region Fields
 
+        [Header("Text")]
+        // reference to gameEndText
+        public TMP_Text gameEndText;
 
+        [Header("Panels")]
+        // reference to lose and win panel
+        public GameObject losePanel;
+        public GameObject winPanel;
+
+        #endregion
+        
         // Start is called before the first frame update
         void Start()
         {
-        
+            
         }
 
         // Update is called once per frame
         void Update()
         {
-        
+            
         }
     }
 }
