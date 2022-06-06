d2a467a6311cb13926420b82939884d0babd8a93
Lincoln Thai <lincoln.thai.gamedesign@gmail.com>
2022-06-06 16:49:01 +1000

    Settings

--- Assets/NASAnal Space Station/Scripts/SettingsApplier.cs
+++ Assets/NASAnal Space Station/Scripts/SettingsApplier.cs
@@ -14,17 +14,26 @@ namespace NASAnalSpaceStation
         public static SettingsApplier instance;
 
         // variables to store settings values
-        // othewr settings
+        // other settings
         public int setResIndex;
         public int setQualityIndex;
+        public bool setFullscreen;
 
         // volume settings
         public float setVolume;
         public float setBGMvol;
         public float setSFXvol;
 
+        // int for fullscreen
+        public int fullscreenStat;
+
+        // other variables
         // reference to0 audiomixer
         public AudioMixer audioMixer;
+
+        // array of resolutions for the platform
+        Resolution[] resolutions;
+
 
         #endregion
 
@@ -46,6 +55,23 @@ namespace NASAnalSpaceStation
 
             // does not get destroyed when loading new scene.
             DontDestroyOnLoad(gameObject);
+
+            // collect resolution option in list/array
+            resolutions = Screen.resolutions;
+
+            // variable that holds the index of current resolution
+            int currentResolutionIndex = 0;
+
+            // loop through each element is our resolutions array
+            for (int i = 0; i < resolutions.Length; i++)
+            {
+                // looks for the current resolution of screen/window
+                if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+                {
+                    // sets current resolution idex for display
+                    currentResolutionIndex = i;
+                }
+            }
         }
 
         // Update is called once per frame
@@ -57,9 +83,18 @@ namespace NASAnalSpaceStation
             PlayerPrefs.GetFloat("volume", setVolume);
             PlayerPrefs.GetFloat("BGMvolume", setBGMvol);
             PlayerPrefs.GetFloat("SFXvolume", setSFXvol);
-            
-            // Call functions
+            PlayerPrefs.GetInt("FullscreenStat", fullscreenStat);
 
+            // call conversion function
+            setFullscreen = intTobool(fullscreenStat);
+
+            // Call functions to apply settings
+            SetResolution(setResIndex);
+            SetQuality(setQualityIndex);
+            SetVolume(setVolume);
+            SetBGMVolume(setBGMvol);
+            SetSFXVolume(setSFXvol);
+            SetFullscreen(setFullscreen);
         }
 
         #endregion
@@ -68,52 +103,55 @@ namespace NASAnalSpaceStation
 
         public void SetResolution(int resolutionIndex)
         {
-            // using the resolution index set in dropdown, look for the resolution to set to this variable resolution
+            // using the resolution index, look for the resolution to set to this variable resolution
             Resolution resolution = resolutions[resolutionIndex];
 
             // sets the resolution
             Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
-
-            // store value ion player prefs
-            PlayerPrefs.SetInt("ResIndex", resolutionIndex);
         }
 
         public void SetVolume(float volume)
         {
             // edits the value of volume in the main mixer e.g Master
             audioMixer.SetFloat("volume", volume);
-
-            // store value in player prefs
-            PlayerPrefs.SetFloat("Volume", volume);
         }
 
         public void SetBGMVolume(float volume)
         {
             // edits the value of volume for BGM in mainh mixer
             audioMixer.SetFloat("BGMvolume", volume);
-
-            // store value in player prefs
-            PlayerPrefs.SetFloat("BGMvolume", volume);
         }
 
         public void SetSFXVolume(float volume)
         {
             // edits the value of volume for SFX in mainh mixer
             audioMixer.SetFloat("SFXvolume", volume);
-
-            // store value in player prefs
-            PlayerPrefs.SetFloat("SFXvolume", volume);
         }
 
         public void SetQuality(int qualityIndex)
         {
             // Adjusts grphics quality
             QualitySettings.SetQualityLevel(qualityIndex);
-
-            // store value ion player prefs
-            PlayerPrefs.SetInt("Quality", qualityIndex);
         }
 
-        #endregion
+        public void SetFullscreen(bool isFullscreen)
+        {
+            // turns on or off fullscreen
+            Screen.fullScreen = isFullscreen;
+        }
+
+        bool intTobool(int val)
+        {
+            if (val != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+            #endregion
+        }
     }
-}
